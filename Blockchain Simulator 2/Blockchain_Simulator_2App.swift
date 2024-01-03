9ece515613e1ba9c2f5dc9f7bd7b3a7b636ac208
R. Kukuh <rkukuh@gmail.com>
2024-01-03 12:15:50 +0700

    Initial Commit

---
+++ Blockchain Simulator 2/Blockchain_Simulator_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Blockchain_Simulator_2App.swift
+//  Blockchain Simulator 2
+//
+//  Created by R. Kukuh on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Blockchain_Simulator_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
