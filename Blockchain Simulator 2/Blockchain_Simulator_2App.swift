4286260e0496b680484356230f22795b40ed68a1
R. Kukuh <rkukuh@gmail.com>
2024-01-04 23:50:35 +0700

    Working WIP

--- Blockchain Simulator 2/Blockchain_Simulator_2App.swift
+++ Blockchain Simulator 2/Blockchain_Simulator_2App.swift
@@ -8,10 +8,21 @@
 import SwiftUI
 
 @main
-struct Blockchain_Simulator_2App: App {
+struct YourApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
-            MainView()
+            MainView() // Replace with your main view
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        if let window = NSApplication.shared.windows.first {
+            window.setFrame(NSRect(x: 0, y: 0, width: 800, height: 600), display: true)
+            window.center()
         }
     }
 }
